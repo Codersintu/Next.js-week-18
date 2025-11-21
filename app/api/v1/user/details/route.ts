d39c5fba4d46e707b8c2c40e1e46c3b522eabdf6
Codersintu <skmahto9199@gmail.com>
2025-11-21 12:54:21 +0530

    added prisma with nextjs

--- app/api/v1/user/details/route.ts
+++ app/api/v1/user/details/route.ts
@@ -1,7 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import { PrismaClient } from "@prisma/client";
+const client=new PrismaClient();
 export async function POST(req:NextRequest){
    const data=await req.json();
+   await client.user.create({data:{
+    username: data.username,
+    password: data.password
+   }});
+   
    console.log("Received user details:", data);
     return NextResponse.json({message: "User details endpoint"})
 }
